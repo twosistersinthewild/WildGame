96a2c177f67ba315b1087e101a2ebd19f749a5b5
raspiz <a.w.whitmer@eagle.clarion.edu>
2015-02-07 01:22:47 -0500

    had to switch width and height values because of the landscape orientation

--- config.lua
+++ config.lua
@@ -2,8 +2,8 @@ application = {
 	content = {
                 -- trying a 3:2 resolution. this fits between 16:9 and 4:3.
                 -- todo: 4:3 may have top and bottom cut off so needs testing
-		width = 960,
-		height = 640, 
+		width = 640,
+		height = 960, 
 		scale = "letterBox",
 		fps = 30,
 		
